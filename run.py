4833a29810ee7d4abbbf45dc41d44697da400b4f
jigar bhanderi <jigarbhanderi@gmail.com>
2023-02-21 13:29:20 +0000

    Data validation

--- run.py
+++ run.py
@@ -31,4 +31,22 @@ def get_customers_data():
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
 
+    cust_data = data_str.split(",")
+    validate_data(cust_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if the string cannot be converted into int,
+    or if there aren't exactly 5 values.
+    """
+    print(values)
+    try: 
+        [int(value) for value in values]
+        if len(values) != 5:
+            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again \n")
+
 get_customers_data()
