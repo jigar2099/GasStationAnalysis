c3a1d4583cec0bc863b3299d7f08c654b03a626f
jigar bhanderi <jigarbhanderi@gmail.com>
2023-02-21 11:51:40 +0000

    wire apis and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('cred.json')
+SCOPE_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open('gas_station_analysis')
+
+customers = SHEET.worksheet('customers')
+
+data = customers.get_all_values()
+print(data)
+
