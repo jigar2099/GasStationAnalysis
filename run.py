b6ad437487f2f0f8da8c38ef51cd8540aa0f5da3
jigar bhanderi <jigarbhanderi@gmail.com>
2023-02-21 13:03:51 +0000

    create the first function to get user data

--- run.py
+++ run.py
@@ -16,5 +16,19 @@ SHEET = GSPREAD_CLIENT.open('gas_station_analysis')
 customers = SHEET.worksheet('customers')
 
 data = customers.get_all_values()
+
 print(data)
 
+
+def get_customers_data():
+    """
+    Get customers data from the user
+    """
+    print("Please enter customer data from the previous entries")
+    print("Data must be five numbers, separated by commas.")
+    print("Example: 1,200,300,400,500 \n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_customers_data()
